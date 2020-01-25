09ff93a7254124fc386198be2345ab0684deb34e
Coder <coder@Coders-MacBook-Pro.local>
2020-01-25 08:50:56 +0800

    siyuan-gao

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -15,6 +15,30 @@
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
+const tyrannosaurus = {
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceous"
+
+}
+
+const stegosaurus = {
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic"
+
+}
+
+const velociraptor = {
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceous"
+
+}
+
 console.log();
 
 // What was the diet of a velociraptor?
